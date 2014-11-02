b0078f2c94dc45b960c7d8de4526fc4fa62fba02
Dionysis Fryganas <dfryganas@gmail.com>
2014-11-02 13:19:23 +0100

    Initial import of OpenCart plugin files.

---
+++ admin/language/english/module/pro6pp.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']       = 'Pro6PP';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module Pro6PP!';
+
+// Entry
+$_['entry_pro6pp_auth_key']          = 'Pro6PP auth_key:<br /><span class="help">Goto <a onclick="window.open(\'http://www.pro6pp.nl\');"><u>the Pro6PP CMS</u></a>, signup and copy &amp; paste the auth_key into the text box.</span>';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module Pro6PP!';
+$_['error_pro6pp_auth_key']          = 'Enter your personal Pro6PP auth_key';
+?>
